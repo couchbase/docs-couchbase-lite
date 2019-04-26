e7801a0f9fdc32c50a518ef3b83645e90f41b748
G. Blake Meike <blake.meike@couchbase.com>
2019-04-26 15:05:15 -0700

    DOC-5129 / https://issues.couchbase.com/browse/DOC-5129

--- modules/ROOT/examples/java/app/src/main/java/com/couchbase/snippets/Examples.java
+++ modules/ROOT/examples/java/app/src/main/java/com/couchbase/snippets/Examples.java
@@ -53,7 +53,10 @@ import com.couchbase.lite.Function;
 import com.couchbase.lite.IndexBuilder;
 import com.couchbase.lite.Join;
 import com.couchbase.lite.ListenerToken;
+import com.couchbase.lite.LogDomain;
+import com.couchbase.lite.LogFileConfiguration;
 import com.couchbase.lite.LogLevel;
+import com.couchbase.lite.Logger;
 import com.couchbase.lite.Message;
 import com.couchbase.lite.MessageEndpoint;
 import com.couchbase.lite.MessageEndpointConnection;
@@ -211,6 +214,15 @@ public class Examples {
         Database.log.getConsole().setLevel(LogLevel.VERBOSE);
         // end::logging[]
     }
+
+    public void testEnableCustomLogging() {
+        // tag::set-custom-logging[]
+        // this custom logger will never be asked to log an event
+        // with a log level < WARNING
+        Database.log.setCustom(new LogTestLogger(LogLevel.WARNING));
+        // end::set-custom-logging[]
+    }
+
 
     // ### File logging
     public void testFileLogging() throws CouchbaseLiteException {
@@ -1185,3 +1197,21 @@ class TensorFlowModel {
 }
 // end::predictive-model[]
 
+// tag::custom-logging[]
+class LogTestLogger implements Logger {
+    @NonNull
+    private final LogLevel level;
+
+    public LogTestLogger(@NonNull LogLevel level) { this.level = level; }
+
+    @NonNull
+    @Override
+    public LogLevel getLevel() { return level; }
+
+    @Override
+    public void log(@NonNull LogLevel level, @NonNull LogDomain domain, @NonNull String message) {
+        // this method will never be called if param level < this.level
+        // handle the message, for example piping it to a third party framework
+    }
+}
+// end::custom-logging[]
