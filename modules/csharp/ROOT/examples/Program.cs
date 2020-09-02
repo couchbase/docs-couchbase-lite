8d3f9dda57cf937f212b6645b364f722405636bd
Sandychuang8 <sandy.chuang@couchbase.com>
2020-09-02 00:46:57 -0700

    DOC-7292 Add Client Cert authenticator with root certs logic code snippets. (#231)
    Note: Resolved merge conflict arising with PR#231
    This snippet overwrote: `private static void TestImportTLSIdentity()` -- IB
    
    
    * DOC-7292 Add Client Cert authenticator with root certs logic code snippets.
    
    * Add comments
    
    Co-authored-by: ibsoln <52778946+ibsoln@users.noreply.github.com>

--- modules/csharp/ROOT/examples/Program.cs
+++ modules/csharp/ROOT/examples/Program.cs
@@ -192,19 +192,61 @@ namespace api_walkthrough
             // end::create-self-signed-cert[]
         }
 
-        private static void TestImportTLSIdentity()
+        private static void TestClientCertAuthenticatorRootCerts()
         {
-            TLSIdentity identity;
-            X509Store _store = new X509Store(StoreName.My); // The identity will be stored in the secure storage using the given label
-            byte[] data = File.ReadAllBytes("C:\\client.p12"); // PKCS12 data containing private key, public key, and certificates
+            var db = new Database("other-database");
+            _Database = db;
 
-            // tag::import-tls-identity[]
-            identity = TLSIdentity.ImportIdentity(_store, 
-                data, 
-                "123", // The password that is needed to access the certificate data
-                "CBL-Client-Cert", 
-                null); // The key label to get cert in certificate map. If null, the same default directory for a Couchbase Lite database is used for map.
-            // end::import-tls-identity[]
+            X509Store _store = new X509Store(StoreName.My);
+            TLSIdentity identity;
+
+            // tag::client-cert-authenticator-root-certs[]
+            byte[] caData, clientData;
+            clientData = File.ReadAllBytes("C:\\client.p12"); // PKCS12 data containing private key, public key, and certificates
+            caData = File.ReadAllBytes("C:\\client-ca.der"); 
+
+            // Root certs
+            var rootCert = new X509Certificate2(caData);
+            var auth = new ListenerCertificateAuthenticator(new X509Certificate2Collection(rootCert));
+
+            // Create URL Endpoint Listener
+            var config = new URLEndpointListenerConfiguration(_Database);
+            config.DisableTLS = false; //The default value is false which means that the TLS will be enabled by default.
+            config.Authenticator = auth;
+            _listener = new URLEndpointListener(config);
+            _listener.Start();
+
+            // Client identity
+            identity = TLSIdentity.ImportIdentity(_store,
+                clientData,
+                "123",
+                "CBL-Client-Cert",
+                null);
+
+            // Replicator -- Client
+            var database = new Database("client-database");
+            var builder = new UriBuilder(
+                "wss",
+                "localhost",
+                _listener.Port,
+                $"/{_listener.Config.Database.Name}"
+            );
+
+            var url = builder.Uri;
+            var target = new URLEndpoint(url);
+            var config = new ReplicatorConfiguration(database, target);
+            config.ReplicatorType = ReplicatorType.PushAndPull;
+            config.Continuous = false;
+            config.Authenticator = new ClientCertificateAuthenticator(identity);
+            config.AcceptOnlySelfSignedServerCertificate = true;
+            config.PinnedServerCertificate = _listener.TlsIdentity.Certs[0];
+            using (var replicator = new Replicator(config)) {
+                replicator.Start();
+            }
+
+            // Stop listener after replicator is stopped
+            _listener.Stop();
+            // end::client-cert-authenticator-root-certs[]
         }
 
         private static void UseEncryption()
