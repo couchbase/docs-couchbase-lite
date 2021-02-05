404291a807a98e6671c65d5000d66e147d06fa18
ibsoln <52778946+ibsoln@users.noreply.github.com>
2021-02-05 18:06:49 +0000

    Simple initialization on ReplicatorConfiguration
    https://github.com/couchbase/docs-couchbase-lite/pull/401#discussion_r570953468

--- modules/objc/examples/code_snippets/SampleCodeTest.m
+++ modules/objc/examples/code_snippets/SampleCodeTest.m
@@ -940,10 +940,10 @@
         id target =
           [[CBLURLEndpoint alloc] initWithURL: [NSURL URLWithString: @"ws://foo.cbl.com/db"]];
 
-        CBLReplicatorConfiguration* config = [self configWithTarget: target
-                                                            type: kCBLReplicatorTypePush
-                                                      continuous: YES];
-
+        CBLReplicatorConfiguration* config = 
+            [[CBLReplicatorConfiguration alloc] initWithDatabase: db target: target];
+        config.type = kCBLReplicatorTypePush;
+        config.continuous: YES];
         config.heartbeat = 60; // <.>
         repl = [[CBLReplicator alloc] initWithConfig: config];
 
