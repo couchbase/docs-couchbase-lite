304488c5ad1526ef20dad793f74dbb46e4448adb
ibsoln <52778946+ibsoln@users.noreply.github.com>
2021-06-03 10:19:21 +0100

    QF210603-1 -- C# example tweak from QF210602-1 (2.8)

--- modules/csharp/examples/code_snippets/Program.cs
+++ modules/csharp/examples/code_snippets/Program.cs
@@ -1671,7 +1671,6 @@ var query =
     {
         // For Documentation
         var dbName = "travel-sample";
-        var this_Db = new Database(dbName) ;
 
         string thisDocsId;
         string thisDocsName;
@@ -1680,9 +1679,11 @@ var query =
         Dictionary<string,object> hotel = new Dictionary<string,object>();
 
         // tag::query-syntax-all[]
+        var this_Db = new Database("hotels") ;
+
         var query = QueryBuilder
               .Select(SelectResult.All())
-              .From(DataSource.Database(this_Db)); // <.>
+              .From(DataSource.Database(this_Db));
 
         // end::query-syntax-all[]
 
@@ -1717,13 +1718,16 @@ var query =
     {
         // For Documentation
         var dbName = "travel-sample";
-        var this_Db = new Database(dbName);
+        // var this_Db = new Database(dbName);
 
         string thisDocsName;
         string thisDocsType;
         string thisDocsCity;
         // tag::query-syntax-props[]
+        var this_Db = new Database("hotels") ;
+
         Dictionary<string, object> hotel = new Dictionary<string, object>();
+
         List<Dictionary<string, object>> hotels = new List<Dictionary<string, object>>();
 
         var query = QueryBuilder.Select(
@@ -1750,7 +1754,6 @@ var query =
 
         }
 
-
     // end::query-access-props[]
     } // test-query-access-props
 
@@ -1760,12 +1763,13 @@ var query =
     {
         // For Documentation
         var dbName = "travel-sample";
-        var this_Db = new Database(dbName);
+        // var this_Db = new Database(dbName);
 
         Dictionary<string, object> hotel = new Dictionary<string, object>();
         List<Dictionary<string, object>> hotels = new List<Dictionary<string, object>>();
 
         // tag::query-syntax-count-only[]
+        var this_Db = new Database("hotels") ;
 
         var query =
           QueryBuilder
@@ -1797,13 +1801,14 @@ var query =
 
         // For Documentation
         var dbName = "travel-sample";
-        var this_Db = new Database(dbName);
+        // var this_Db = new Database(dbName);
 
         Dictionary<string, object> hotel = new Dictionary<string, object>();
         List<Dictionary<string, object>> hotels = new List<Dictionary<string, object>>();
 
 
         // tag::query-syntax-id[]
+        var this_Db = new Database("hotels") ;
 
         var query = QueryBuilder
                 .Select(SelectResult.Expression(Meta.ID).As("this_ID"))
@@ -1830,9 +1835,10 @@ var query =
     {
       // For Documentation
       var dbName = "travel-sample";
-      var this_Db = new Database(dbName);
+      // var this_Db = new Database(dbName);
 
     // tag::query-syntax-pagination[]
+      var this_Db = new Database("hotels") ;
 
       var thisLimit = 20;
       var thisOffset = 0;
