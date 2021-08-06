d13777aaf17bff02164d80ffcc6e60613ba620d6
ibsoln <52778946+ibsoln@users.noreply.github.com>
2021-08-06 15:12:08 +0100

    QF210806-2-nav -- rejig nav structure
    QF210806-2-nav -- r#2ejig nav structure

---
+++ temp-bin/save-8132-210729/modules/swift/examples/code_snippets/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  code-snippets
+//
+//  Created by James Nocentini on 12/02/2019.
+//  Copyright © 2019 couchbase. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
