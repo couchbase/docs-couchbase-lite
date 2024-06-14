8bcb9175cee8573bdcf09a3a9dda663da040d7b1
Pasin Suriyentrakorn <pasin@couchbase.com>
2024-06-14 10:43:45 -0700

    Fix iOS PR validation

--- jenkins/ios.sh
+++ jenkins/ios.sh
@@ -64,6 +64,7 @@ do
 
     # Build snippets app
     TEST_SIMULATOR=$(xcrun xctrace list devices 2>&1 | grep -oE 'iPhone.*?[^\(]+' | head -1 | sed 's/Simulator//g' | awk '{$1=$1;print}')
+    echo "TEST_SIMULATOR=${TEST_SIMULATOR}"
     xcodebuild build -project code_snippets.xcodeproj -scheme "code-snippets" -destination "platform=iOS Simulator,name=${TEST_SIMULATOR}"
     popd
 
