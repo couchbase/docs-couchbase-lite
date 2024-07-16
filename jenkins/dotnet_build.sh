51947d980b8b296b0a218a9726bb2aa4abea8375
Jim Borden <jim.borden@couchbase.com>
2024-07-16 11:32:59 +0900

    Fix typo

--- jenkins/dotnet_build.sh
+++ jenkins/dotnet_build.sh
@@ -7,7 +7,7 @@ cbl_build=$(curl -s "http://proget.build.couchbase.com:8080/api/get_version?prod
 vs_build=$(curl -s "http://proget.build.couchbase.com:8080/api/get_version?product=couchbase-lite-net-vector-search&version=$vs_version&ee=true" | jq .BuildNumber)
 
 pushd modules/csharp/examples/code_snippets
-dotnet add package couchbase.lite.enterprise -v $cbl_version-b$(printf "%04f" $cbl_build)
-dotnet add package couchbase.lite.VectorSearch -v $vs_version-b$(printf "%04f" $vs_build)
+dotnet add package couchbase.lite.enterprise -v $cbl_version-b$(printf "%04d" $cbl_build)
+dotnet add package couchbase.lite.VectorSearch -v $vs_version-b$(printf "%04d" $vs_build)
 dotnet restore
 dotnet build --no-restore -c Release
