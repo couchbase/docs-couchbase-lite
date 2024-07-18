1350271f8880d35236ea295e20f66f841831dc80
Pasin Suriyentrakorn <pasin@couchbase.com>
2024-07-18 09:37:25 -0700

    CBL-5973 : Lazy vector index code snippet for C

--- modules/c/examples/code_snippets/VectorSearch.h
+++ modules/c/examples/code_snippets/VectorSearch.h
@@ -11,6 +11,8 @@ public:
     }
 };
 
+class TransientError : public std::exception { };
+
 class VectorSearch {
 public:
 
@@ -128,6 +130,90 @@ public:
         CBLVectorEncoding_Free(config.encoding);
         CBLCollection_Release(collection);
         // end::vs-create-predictive-index[]
+    }
+
+    void createLazyIndex() {
+        // tag::vs-lazy-index-config[]
+        // Creating a lazy vector index using the document's 'color' key.
+        // The value of this key will be used to compute a vector when updating the index.
+        CBLVectorIndexConfiguration config{};
+        config.expressionLanguage = kCBLN1QLLanguage;
+        config.expression = FLStr("color");
+        config.dimensions = 3;
+        config.centroids = 2;
+        config.isLazy = true;
+        // end::vs-lazy-index-config[]
+    }
+
+    void updateLazyIndex() {
+        CBLDatabase* database = getDatabase();
+        CBLCollection* collection = CBLDatabase_Collection(database, FLStr("colors"), FLStr("_default"), nullptr);
+
+        // tag::vs-create-lazy-index-embedding[]
+        CBLError err {};
+        CBLQueryIndex* index = CBLCollection_GetIndex(collection, FLStr("color_index"), &err);
+        if (!index) { throw std::domain_error("Index Not Found"); }
+
+        while (true) {
+            // Start an update on it (in this case, limit to 50 entries at a time)
+            CBLIndexUpdater* updater = CBLQueryIndex_BeginUpdate(index, 50, &err);
+            if (!updater) {
+                if (err.code != 0) { throw std::domain_error("Error Begin Update"); }
+                // If updater is NULL and no error, that means there are no more entries to process
+                break;
+            }
+
+            for (size_t i = 0; i < CBLIndexUpdater_Count(updater); i++) {
+                // The value type will depend on the expression you have set in your index.
+                // In this example, it is a string property.
+                FLString value = FLValue_AsString(CBLIndexUpdater_Value(updater, i));
+                std::string colorString = std::string((char*)value.buf, value.size);
+
+                std::vector<float> vector;
+                try {
+                    // Call a MLModel to get a vector.
+                    vector = Color::getVector(colorString);
+                } catch (const TransientError& e) {
+                    // Bad connection? Corrupted over the wire? Something bad happened
+                    // and the vector cannot be generated at the moment. So skip
+                    // this entry. The next time CBLQueryIndex_BeginUpdate is called,
+                    // it will be considered again.
+                    CBLIndexUpdater_SkipVector(updater, i);
+                } catch (...) {
+                    // An unexpected error happened.
+                    CBLIndexUpdater_Release(updater);
+                    throw std::domain_error("Error Getting a Vector");
+                }
+
+                bool success;
+                if (!vector.empty()) {
+                    // The size of the vector must match the number of dimensions set in the index.
+                    // Otherwise, an error will be returned.
+                    success = CBLIndexUpdater_SetVector(updater, i, vector.data(),vector.size(),  &err);
+                } else {
+                    // No vector applicable. Calling SetVector with NULL will
+                    // cause the underlying document to NOT be indexed
+                    success = CBLIndexUpdater_SetVector(updater, i, nullptr, 0, &err);
+                }
+                if (!success) {
+                    CBLIndexUpdater_Release(updater);
+                    throw std::domain_error("Error Setting a Vector");
+                }
+            }
+
+            // This writes the vectors to the index. You MUST have either set or
+            // skipped all the values inside the updater or this call will return an error.
+            if (!CBLIndexUpdater_Finish(updater, &err)) {
+                CBLIndexUpdater_Release(updater);
+                throw std::domain_error("Error Finish Updating");
+            }
+
+            CBLIndexUpdater_Release(updater);
+        }
+
+        CBLQueryIndex_Release(index);
+
+        // end::vs-create-lazy-index-embedding[]
     }
 
     void queryUsingVectorMatch() {
