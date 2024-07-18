8f18517f38854feb0915eaa522a24adc8191c745
Pasin Suriyentrakorn <pasin@couchbase.com>
2024-07-18 13:17:14 -0700

    CBL-5974 : Lazy Vector Index Snippet for Swift

--- modules/swift/examples/code_snippets/VectorSearch.swift
+++ modules/swift/examples/code_snippets/VectorSearch.swift
@@ -12,11 +12,20 @@ var database: Database!
 var collection: Collection!
 
 enum AppError: Error {
+    case indexdNotFound
     case vectorNotFound
+}
+
+enum ColorError: Error {
+    case transient
 }
 
 class Color {
     static func getVector(color: String) throws -> [Float]? {
+        return []
+    }
+    
+    static func getVectorAsync(color: String) async throws -> [Float]? {
         return []
     }
 }
@@ -40,8 +49,8 @@ class VectorSearchSnippets {
     func createDefaultVectorIndexConfig() throws {
         // tag::vs-create-default-config[]
         // Create a vector index configuration for indexing 3-dimensional vectors embedded
-        // in the documents' key named "vector" using 2 centroids.
-        var config = VectorIndexConfiguration(expression: "vector", dimensions: 3, centroids: 2)
+        // in the documents' key named "vector" with 100 centroids.
+        let config = VectorIndexConfiguration(expression: "vector", dimensions: 3, centroids: 100)
         // end::vs-create-default-config[]
         try collection.createIndex(withName: "colors_index", config: config)
     }
@@ -49,7 +58,7 @@ class VectorSearchSnippets {
     func createCustomVectorIndexConfig() throws {
         // tag::vs-create-custom-config[]
         // Create a vector index configuration for indexing 3-dimensional vectors embedded in
-        // the documents' key named "vector" using 100 centroids. The configuration customizes
+        // the documents' key named "vector" with 100 centroids. The configuration customizes
         // the encoding, the distance metric, the number of probes, and the training size.
         var config = VectorIndexConfiguration(expression: "vector", dimensions: 3, centroids: 100)
         config.encoding = .none
@@ -65,7 +74,7 @@ class VectorSearchSnippets {
         // tag::vs-create-index[]
         // Create a vector index configuration from a document property named "vector" which
         // contains the vector embedding.
-        let config = VectorIndexConfiguration(expression: "vector", dimensions: 3, centroids: 2)
+        let config = VectorIndexConfiguration(expression: "vector", dimensions: 3, centroids: 100)
         try collection.createIndex(withName: "colors_index", config: config)
         // end::vs-create-index[]
     }
@@ -151,10 +160,60 @@ class VectorSearchSnippets {
         // Create a vector index configuration with an expression using the prediction
         // function to get the vectors from the registered predictive model.
         let expression = "prediction(WordEmbedding, {\"colorInput\": color}).vector"
-        let config = VectorIndexConfiguration(expression: expression, dimensions: 300, centroids: 8)
+        let config = VectorIndexConfiguration(expression: expression, dimensions: 3, centroids: 100)
         
         // Create vector index from the configuration
         try collection.createIndex(withName: "colors_index", config: config)
     }
     // end::vs-create-predictive-index[]
+    
+    func createLazyIndex() throws {
+        // tag::vs-lazy-index-config[]
+        // Creating a lazy vector index using the document's 'color' key.
+        // The value of this key will be used to compute a vector when updating the index.
+        var config = VectorIndexConfiguration(expression: "color", dimensions: 3, centroids: 100)
+        config.isLazy = true;
+        // end::vs-lazy-index-config[]
+    }
+    
+    func updateLazyIndex() async throws {
+        // tag::vs-create-lazy-index-embedding[]
+        guard let index = try collection.index(withName: "colors_index") else {
+            throw AppError.indexdNotFound
+        }
+        
+        while (true) {
+            // Start an update on it (in this case, limit to 50 entries at a time)
+            guard let updater = try index.beginUpdate(limit: 50) else {
+                // If updater is nil and no error, that means there are no more entries to process
+                break
+            }
+            
+            for i in 0..<updater.count {
+                // The value type will depend on the expression you have set in your index.
+                // In this example, it is a string property.
+                let color = updater.string(at: i)!
+                
+                var vector: [Float]? = nil
+                do {
+                    vector = try await Color.getVectorAsync(color: color)
+                } catch ColorError.transient {
+                    // Bad connection? Corrupted over the wire? Something bad happened
+                    // and the vector cannot be generated at the moment. So skip
+                    // this entry. The next time CBLQueryIndex_BeginUpdate is called,
+                    // it will be considered again.
+                    updater.skipVector(at: i)
+                }
+                
+                // Set the computed vector here. If vector is nil, calling setVector
+                // will cause the underlying document to NOT be indexed.
+                try updater.setVector(vector, at: i)
+            }
+            
+            // This writes the vectors to the index. You MUST have either set or
+            // skipped all the values inside the updater or this call will throw an error.
+            try updater.finish()
+        }
+        // end::vs-create-lazy-index-embedding[]
+    }
 }
