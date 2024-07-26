0909cfa762648b361b263761a30dacbc2b22d1ac
Pasin Suriyentrakorn <pasin@couchbase.com>
2024-07-26 04:17:28 -0700

    CBL-5968 and CBL-5973 : numProbes and lazy index snippet for C (#883)
    * CBL-5968 : Add numProbes snippet and change to use the latest API for C
    
    * Added numProbes to vs-create-custom-config tag. Adjust number of centroids and training size to make it inline with the guideline.
    
    * Changed to use the latest API and SQL Syntax.
    
    * Fixed error in createVectorIndexWithPredictiveModel.
    
    * CBL-5973 : Lazy vector index code snippet for C

--- modules/c/examples/code_snippets/VectorSearch.h
+++ modules/c/examples/code_snippets/VectorSearch.h
@@ -11,13 +11,15 @@ public:
     }
 };
 
+class TransientError : public std::exception { };
+
 class VectorSearch {
 public:
 
     void enableVectorSearchExtension()
     {
         // tag::vs-setup-packaging[]
-        CBL_SetExtensionPath(FLStr("/path/to/extension_dir"));
+        CBL_EnableVectorSearch(FLStr("/path/to/extension_dir"));
         // end::vs-setup-packaging[]
     }
 
@@ -29,25 +31,26 @@ public:
         config.expressionLanguage = kCBLN1QLLanguage;
         config.expression = FLStr("vector");
         config.dimensions = 3;
-        config.centroids = 2;
+        config.centroids = 100;
         // end::vs-create-default-config[]
     }
 
     void createCustomVectorIndexConfig() {
         // tag::vs-create-custom-config[]
-        // Create a vector index configuration for indexing 3-dimensional vectors embedded
-        // in the documents' key named "vector" using 2 centroids. The config is customized
-        // to use Cosise distance metric, no vector encoding, min training size 100 and
-        // max training size 200.
+        // Create a vector index configuration for indexing 3-dimensional vectors embedded in
+        // the documents' key named "vector" using 100 centroids. The configuration customizes
+        // the encoding, the distance metric, the number of probes, and the training size.
+        // Note: Free the created encoding using CBLVectorEncoding_Free after creating the index.
         CBLVectorIndexConfiguration config{};
         config.expressionLanguage = kCBLN1QLLanguage;
         config.expression = FLStr("vector");
         config.dimensions = 3;
-        config.centroids = 2;
-        // Note: Calls CBLVectorEncoding_Free(config.encoding) after creating the index.
+        config.centroids = 100;
+        config.metric = kCBLDistanceMetricCosine;
+        config.numProbes = 8;
         config.encoding = CBLVectorEncoding_CreateNone();
-        config.minTrainingSize = 100;
-        config.maxTrainingSize = 200;
+        config.minTrainingSize = 2500;
+        config.maxTrainingSize = 5000;
         // end::vs-create-custom-config[]
     }
 
@@ -59,8 +62,7 @@ public:
         config.expressionLanguage = kCBLN1QLLanguage;
         config.expression = FLStr("vector");
         config.dimensions = 3;
-        config.centroids = 2;
-        config.encoding = CBLVectorEncoding_CreateNone();
+        config.centroids = 100;
 
         CBLError err {};
         CBLCollection* collection = CBLDatabase_Collection(database, FLStr("colors"), FLStr("_default"), &err);
@@ -69,7 +71,6 @@ public:
         bool result = CBLCollection_CreateVectorIndex(collection, FLStr("colors_index"), config, &err);
         if (!result) { throw std::domain_error("Create Index Error"); }
 
-        CBLVectorEncoding_Free(config.encoding);
         CBLCollection_Release(collection);
         // end::vs-create-index[]
     }
@@ -78,7 +79,7 @@ public:
         CBLDatabase* database = getDatabase();
 
         // tag::vs-predictive-model[]
-        auto callback = [](void* context, FLDict input) -> FLSliceResult {
+        auto callback = [](void* context, FLDict input) -> FLMutableDict {
             // Set color input string
             FLString color = FLValue_AsString(FLDict_Get(input, FLStr("colorInput")));
 
@@ -86,19 +87,17 @@ public:
             auto colorString = std::string(static_cast<const char *>(color.buf), color.size);
             std::vector<float> colorVector = Color::getVector(colorString);
 
-            // Construct output as Fleece Dictionary Encoded:
+            // Construct a fleece array for the color vector:
             FLMutableArray vectorArray = FLMutableArray_New();
             for (float val : colorVector) {
                 FLMutableArray_AppendFloat(vectorArray, val);
             }
 
-            FLEncoder enc = FLEncoder_New();
-            FLEncoder_BeginDict(enc, 1);
-            FLEncoder_WriteKey(enc, FLStr("vector_output"));
-            FLEncoder_WriteValue(enc, (FLValue)vectorArray);
-            FLEncoder_EndDict(enc);
+            // Construct a fleece dictionary output
+            FLMutableDict output = FLMutableDict_New();
+            FLMutableDict_SetArray(output, FLStr("vector"), vectorArray);
             FLMutableArray_Release(vectorArray);
-            return FLEncoder_Finish(enc, nullptr);
+            return output;
         };
 
         CBLPredictiveModel model {};
@@ -113,7 +112,7 @@ public:
         // function to get the vectors from the registered predictive model.
         CBLVectorIndexConfiguration config {};
         config.expressionLanguage = kCBLN1QLLanguage;
-        config.expression = FLStr("predict(ColorModel, {\"colorInput\": color})");
+        config.expression = FLStr("prediction(ColorModel, {\"colorInput\": color}).vector");
         config.dimensions = 3;
         config.centroids = 2;
         config.encoding = CBLVectorEncoding_CreateNone();
@@ -130,14 +129,98 @@ public:
         // end::vs-create-predictive-index[]
     }
 
-    void queryUsingVectorMatch() {
+    void createLazyIndex() {
+        // tag::vs-lazy-index-config[]
+        // Creating a lazy vector index using the document's 'color' key.
+        // The value of this key will be used to compute a vector when updating the index.
+        CBLVectorIndexConfiguration config{};
+        config.expressionLanguage = kCBLN1QLLanguage;
+        config.expression = FLStr("color");
+        config.dimensions = 3;
+        config.centroids = 100;
+        config.isLazy = true;
+        // end::vs-lazy-index-config[]
+    }
+
+    void updateLazyIndex() {
+        CBLDatabase* database = getDatabase();
+        CBLCollection* collection = CBLDatabase_Collection(database, FLStr("colors"), FLStr("_default"), nullptr);
+
+        // tag::vs-create-lazy-index-embedding[]
+        CBLError err {};
+        CBLQueryIndex* index = CBLCollection_GetIndex(collection, FLStr("colors_index"), &err);
+        if (!index) { throw std::domain_error("Index Not Found"); }
+
+        while (true) {
+            // Start an update on it (in this case, limit to 50 entries at a time)
+            CBLIndexUpdater* updater = CBLQueryIndex_BeginUpdate(index, 50, &err);
+            if (!updater) {
+                if (err.code != 0) { throw std::domain_error("Error Begin Update"); }
+                // If updater is NULL and no error, that means there are no more entries to process
+                break;
+            }
+
+            for (size_t i = 0; i < CBLIndexUpdater_Count(updater); i++) {
+                // The value type will depend on the expression you have set in your index.
+                // In this example, it is a string property.
+                FLString value = FLValue_AsString(CBLIndexUpdater_Value(updater, i));
+                std::string colorString = std::string((char*)value.buf, value.size);
+
+                std::vector<float> vector;
+                try {
+                    // Call a MLModel to get a vector.
+                    vector = Color::getVector(colorString);
+                } catch (const TransientError& e) {
+                    // Bad connection? Corrupted over the wire? Something bad happened
+                    // and the vector cannot be generated at the moment. So skip
+                    // this entry. The next time CBLQueryIndex_BeginUpdate is called,
+                    // it will be considered again.
+                    CBLIndexUpdater_SkipVector(updater, i);
+                } catch (...) {
+                    // An unexpected error happened.
+                    CBLIndexUpdater_Release(updater);
+                    throw std::domain_error("Error Getting a Vector");
+                }
+
+                bool success;
+                if (!vector.empty()) {
+                    // The size of the vector must match the number of dimensions set in the index.
+                    // Otherwise, an error will be returned.
+                    success = CBLIndexUpdater_SetVector(updater, i, vector.data(),vector.size(),  &err);
+                } else {
+                    // No vector applicable. Calling SetVector with NULL will
+                    // cause the underlying document to NOT be indexed
+                    success = CBLIndexUpdater_SetVector(updater, i, nullptr, 0, &err);
+                }
+                if (!success) {
+                    CBLIndexUpdater_Release(updater);
+                    throw std::domain_error("Error Setting a Vector");
+                }
+            }
+
+            // This writes the vectors to the index. You MUST have either set or
+            // skipped all the values inside the updater or this call will return an error.
+            if (!CBLIndexUpdater_Finish(updater, &err)) {
+                CBLIndexUpdater_Release(updater);
+                throw std::domain_error("Error Finish Updating");
+            }
+
+            CBLIndexUpdater_Release(updater);
+        }
+
+        CBLQueryIndex_Release(index);
+
+        // end::vs-create-lazy-index-embedding[]
+    }
+
+    void queryVectorSearch() {
         CBLDatabase* database = getDatabase();
 
         // tag::vs-use-vector-match[]
-        // Create a query to search similar colors by using the vector_match()
-        // function in the vector index named "colors_index".
+        // Create a query to search similar colors by using the approx_vector_distance() function
+        // in the ORDER BY clause.
         CBLError err{};
-        const char* sql = "SELECT id, color FROM _default.colors WHERE vector_match(colors_index, $vector, 8)";
+        const char* sql = "SELECT id, color FROM _default.colors ORDER BY approx_vector_distance(vector, $vector) LIMIT 8";
         CBLQuery* query = CBLDatabase_CreateQuery(database, kCBLN1QLLanguage,
                                                   FLStr(sql),
                                                   nullptr, &err);
@@ -171,13 +254,13 @@ public:
         // end::vs-use-vector-match[]
     }
 
-    void queryUsingVectorDistance() {
+    void queryVectorDistance() {
         CBLDatabase* database = getDatabase();
 
         // tag::vs-use-vector-distance[]
-        // Create a query to get vector distances using the vector_distance() function.
+        // Create a query to get vector distances using the approx_vector_distance() function.
         CBLError err{};
-        const char* sql = "SELECT id, color, vector_distance(colors_index) FROM _default.colors WHERE vector_match(colors_index, $vector, 8)";
+        const char* sql = "SELECT id, color, approx_vector_distance(vector, $vector) FROM _default.colors LIMIT 8";
         CBLQuery* query = CBLDatabase_CreateQuery(database, kCBLN1QLLanguage,
                                                   FLStr(sql),
                                                   nullptr, &err);
