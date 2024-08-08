dc38a77a2a9a981d524f197f0a5cb3fa6f07f84c
Jim Borden <jim.borden@couchbase.com>
2024-08-09 08:14:05 +0900

    Update C# validation to pull specific version

--- Jenkinsfile
+++ Jenkinsfile
@@ -9,7 +9,7 @@ pipeline {
                 stage("Validate C#") {
                     agent { label 's61113u16 (litecore)' }
                     steps {
-                        sh 'jenkins/dotnet_build.sh'
+                        sh 'jenkins/dotnet_build.sh 3.1.9'
                     }
                 }
                 stage("Validate C") {
--- jenkins/dotnet_build.sh
+++ jenkins/dotnet_build.sh
@@ -1,6 +1,10 @@
 #!/bin/bash -e
 
+cbl_version=${1:-3.1.0}
+
+cbl_build=$(curl -s "http://proget.build.couchbase.com:8080/api/get_version?product=couchbase-lite-net&version=$cbl_version&ee=true" | jq .BuildNumber)
+
 pushd modules/csharp/examples/code_snippets
-dotnet add package --prerelease couchbase.lite.enterprise
+dotnet add package couchbase.lite.enterprise -v $cbl_version-b$(printf "%04d" $cbl_build)
 dotnet restore
 dotnet build --no-restore -c Release
