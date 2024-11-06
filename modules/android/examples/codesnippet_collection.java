16d7bfa2045478bfdf9f67d8dbe6eb4f00968106
G. Blake Meike <blake.meike@couchbase.com>
2024-11-06 03:00:04 -0800

    CBL-6394: JAK Array Index snippets (#927)

--- modules/android/examples/codesnippet_collection.java
+++ modules/android/examples/codesnippet_collection.java
@@ -236,6 +236,51 @@ public class SnippetApplication extends Application {
     }
 }
 //
+// Copyright (c) 2024 Couchbase, Inc All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+package com.couchbase.codesnippets;
+
+import com.couchbase.lite.ArrayIndexConfiguration;
+import com.couchbase.lite.Collection;
+import com.couchbase.lite.CouchbaseLiteException;
+import com.couchbase.lite.IndexConfiguration;
+
+
+public class ArrayIndexExamples {
+    public void ArrayIndexConfig() {
+        // tag::array-index-config[]
+        IndexConfiguration config = new ArrayIndexConfiguration("contacts", "type");
+        // end::array-index-config[]
+    }
+
+    public void ArrayIndexSingle(Collection collection) throws CouchbaseLiteException {
+        // tag::array-index-single[]
+        collection.createIndex("myindex", new ArrayIndexConfiguration("likes"));
+        // end::array-index-single[]
+    }
+
+    public void ArrayIndexNested(Collection collection) throws CouchbaseLiteException {
+
+        // tag::array-index-nested[]
+        collection.createIndex(
+            "myindex",
+            new ArrayIndexConfiguration("contacts[].phones", "type"));
+        // end::array-index-nested[]
+    }
+}
+//
 // Copyright (c) 2023 Couchbase, Inc All rights reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
