95adbcbd971f3756949c17b20ecbd67a15bfcb69
Jim Borden <jim.borden@couchbase.com>
2025-02-07 11:04:46 +0900

    CBL-6748: Add new logging API snippets
    Added two more tags since to demonstrate custom logging you need to show the class being used

--- modules/csharp/examples/code_snippets/Program.cs
+++ modules/csharp/examples/code_snippets/Program.cs
@@ -1804,7 +1804,7 @@ namespace api_walkthrough
             // end::tojson-blob[]
         }
 
-        private void CreateArrayIndex()
+        public void CreateArrayIndex()
         {
             var database = new Database("my-database");
             var collection = database.GetDefaultCollection();
@@ -2601,5 +2601,73 @@ public class MyClass
         // Add collections to the config now
 
         // end::sgw-act-rep-initialize[]
+    }
+
+    // tag::custom-logger
+    internal class MyCoolCustomLogger : ILogger
+    {
+        public LogLevel Level { get; set; }
+
+        public void Log(LogLevel level, LogDomain domain, string message)
+        {
+            // Do something cool with this information
+        }
+    }
+    // end::custom-logger
+
+    // tag::custom-log-sink
+    internal class MyCoolLogSink : BaseLogSink
+    {
+        public MyCoolLogSink(LogLevel level)
+            : base(level)
+        {
+
+        }
+
+        protected override void WriteLog(LogLevel level, LogDomain domain, string message)
+        {
+            // Do something cool with this information
+        }
+    }
+    // end::custom-log-sink
+
+    public void OldLoggingApi()
+    {
+        // tag::console-logging[]
+        Database.Log.Console.Level = LogLevel.Verbose;
+        // end::console-logging[]
+
+        // tag::file-logging[]
+        Database.Log.File.Config = new LogFileConfiguration("path/to/log/directory")
+        {
+            MaxRotateCount = 2, // Save 3 log files (i.e. 2 rotated and 1 current)
+            MaxSize = 1024 * 512, // 512KB per file, then rotated
+        };
+
+        Database.Log.File.Level = LogLevel.Verbose;
+        // end::file-logging[]
+
+        // tag::custom-logging[]
+        Database.Log.Custom = new MyCoolCustomLogger() { Level = LogLevel.Verbose };
+        // end::custom-logging[]
+    }
+
+    public void NewLoggingApi()
+    {
+        // tag::new-console-logging[]
+        LogSinks.Console = new ConsoleLogSink(LogLevel.Verbose);
+        // end::new-console-logging[]
+
+        // tag::new-file-logging[]
+        LogSinks.File = new FileLogSink(LogLevel.Verbose, "path/to/log/directory")
+        {
+            MaxKeptFiles = 3, // Save 3 log files (i.e. 2 rotated and 1 current)
+            MaxSize = 1024 * 512, // 512KB per file, then rotated
+        };
+        // end::new-file-logging[]
+
+        // tag::new-custom-logging[]
+        LogSinks.Custom = new MyCoolLogSink(LogLevel.Verbose);
+        // end::new-custom-logging[]
     }
 }
