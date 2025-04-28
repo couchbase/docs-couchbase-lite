0db87ab7ae5ce7125c17dff05102d90f2c919d9f
Vlad Velicu <vlad.velicu@couchbase.com>
2025-04-28 17:52:45 +0100

    init

---
+++ modules/swift/examples/code_snippets/CodableCombine.swift
@@ -0,0 +1,17 @@
+//
+//  CodableCombine.swift
+//  CouchbaseLite
+//
+//  Copyright © 2025 couchbase. All rights reserved.
+//
+
+import Foundation
+import CouchbaseLiteSwift
+
+class CodableCombine {
+    var database: Database!
+    var collection: Collection!
+    var replicator: Replicator!
+}
+
+
