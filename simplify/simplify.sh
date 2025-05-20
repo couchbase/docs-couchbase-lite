aa86ca280c25f904746392b6bee1f664a9ff8508
Hakim Cassimally <hakim.cassimally@couchbase.com>
2025-05-20 17:33:20 +0100

    updates fixing the indexing pages

--- simplify/simplify.sh
+++ simplify/simplify.sh
@@ -20,7 +20,7 @@ ln -f $SCRIPT_DIR/attributes.pl .
 ln -f $SCRIPT_DIR/rename.sh .
 
 ## UNCOMMENT THIS to run Antora with the assembler feature 
-# npx antora mobile.yml --log-level fatal
+npx antora mobile.yml --log-level fatal
 
 process() {
     WHAT=$1
