29ed32ba9c286142272d0992dc7046ab00dbd29b
Fortune Ikechi <fortune.ikechi@couchbase.com>
2025-07-25 12:17:15 +0100

    update include code samples tag

--- modules/swift/examples/code_snippets/MultipeerReplicator.swift
+++ modules/swift/examples/code_snippets/MultipeerReplicator.swift
@@ -13,50 +13,50 @@ class MultipeerReplicatorSnippets {
     var collection1: Collection!
     var collection2: Collection!
     var collection3: Collection!
-    
+
     func collectionSimple() throws -> [MultipeerCollectionConfiguration] {
-        // tag::multipeer-collection-simple
+        // tag::multipeer-collection-simple[]
         let collections = [collection1, collection2, collection3].map {
             MultipeerCollectionConfiguration(collection: $0)
         }
-        // end::multipeer-collection-simple
+        // end::multipeer-collection-simple[]
         return collections
     }
-    
+
     func collectionConfig() throws -> [MultipeerCollectionConfiguration] {
-        // tag::multipeer-collection-config
+        // tag::multipeer-collection-config[]
         class CustomConflictResolver: MultipeerConflictResolver {
             func resolve(peerID: PeerID, conflict: Conflict) -> Document? {
                 return conflict.remoteDocument
             }
         }
-            
+
         // Create a collection config with a conflict resolver
         var config1 = MultipeerCollectionConfiguration(collection: collection1)
         config1.conflictResolver = CustomConflictResolver()
-        
+
         // Create a collection config with a document ID filter
         var config2 = MultipeerCollectionConfiguration(collection: collection2)
         config2.documentIDs = ["doc1", "doc2"]
-        
+
         // Create a collection config with a push replication filter
         var config3 = MultipeerCollectionConfiguration(collection: collection3)
         config3.pushFilter = { peerID, document, flags in
             return document.int(forKey: "access-level") == 2
         }
-        
+
         let collections = [config1, config2, config3]
-        // end::multipeer-collection-config
+        // end::multipeer-collection-config[]
         return collections
     }
-    
+
     func peerIdentity() throws -> TLSIdentity {
-        // tag::multipeer-tlsidentity
+        // tag::multipeer-tlsidentity[]
         let persistentLabel = "com.myapp.identity"
-    
+
         // Retrieve the TLS identity from the keychain using the persistent label.
         var identity = try TLSIdentity.identity(withLabel: persistentLabel)
-        
+
         // If the identity exists but is expired, delete it.
         if let existing = identity, existing.expiration < Date() {
             try TLSIdentity.deleteIdentity(withLabel: persistentLabel)
@@ -71,11 +71,11 @@ class MultipeerReplicatorSnippets {
             let issuer = try TLSIdentity.createIdentity(
                 withPrivateKey: privateKey,
                 certificate: cert)
-            
+
             // Define certificate attributes and expiration date.
             let attrs: [String: String] = [certAttrCommonName: "MyApp"]
             let expiration = Calendar.current.date(byAdding: .year, value: 2, to: Date())!
-            
+
             // Create a new identity signed with the issuer and save it with the persistent label.
             identity = try TLSIdentity.createIdentity(
                 for: [.clientAuth, .serverAuth],
@@ -84,99 +84,99 @@ class MultipeerReplicatorSnippets {
                 issuer: issuer,
                 label: persistentLabel)
         }
-        // end::multipeer-tlsidentity
+        // end::multipeer-tlsidentity[]
         return identity!
     }
-    
+
     func getIssuerPrivateKeyData() throws -> Data {
         return Data(base64Encoded: "your_base64_encoded_private_key")!
     }
-    
+
     func getIssuerCertificateData() throws -> Data {
         return Data(base64Encoded: "your_base64_encoded_certicate")!
     }
-    
+
     func authenticatorWithCallback() throws -> MultipeerAuthenticator{
-        // tag::multipeer-authenticator-callback
+        // tag::multipeer-authenticator-callback[]
         let authenticator = MultipeerCertificateAuthenticator { peerID, certs in
             return true
         }
-        // end::multipeer-authenticator-callback
+        // end::multipeer-authenticator-callback[]
         return authenticator
     }
-    
+
     func authenticatorWithRootCerts() throws -> MultipeerAuthenticator {
         let privateKey = try getIssuerPrivateKeyData()
         let cert = try getIssuerCertificateData()
         let issuer = try TLSIdentity.createIdentity(withPrivateKey: privateKey, certificate: cert)
-        
-        // tag::multipeer-authenticator-rootcerts
+
+        // tag::multipeer-authenticator-rootcerts[]
         let authenticator = MultipeerCertificateAuthenticator(rootCerts: issuer.certs)
-        // end::multipeer-authenticator-rootcerts
+        // end::multipeer-authenticator-rootcerts[]
         return authenticator
     }
-    
+
     func createConfig() throws -> MultipeerReplicatorConfiguration {
         let identity = try peerIdentity()
         let authenticator = try authenticatorWithRootCerts()
         let collections = try collectionConfig()
-        
-        // tag::multipeer-config
+
+        // tag::multipeer-config[]
         let config = MultipeerReplicatorConfiguration(
             peerGroupID: "com.myapp",
             identity: identity,
             authenticator: authenticator,
             collections: collections)
-        // end::multipeer-config
+        // end::multipeer-config[]
         return config
     }
-    
+
     func createMultipeerReplicator() throws -> MultipeerReplicator {
         let config = try createConfig()
-        // tag::multipeer-replicator
+        // tag::multipeer-replicator[]
         let replicator = try MultipeerReplicator(config: config)
-        // end::multipeer-replicator
+        // end::multipeer-replicator[]
         return replicator
     }
-    
+
     func startReplicator() throws {
         let replicator = try createMultipeerReplicator()
-        // tag::multipeer-replicator-start
+        // tag::multipeer-replicator-start[]
         replicator.start()
-        // end::multipeer-replicator-start
+        // end::multipeer-replicator-start[]
     }
-    
+
     func stopReplicator() throws {
         let replicator = try createMultipeerReplicator()
-        // tag::multipeer-replicator-stop
+        // tag::multipeer-replicator-stop[]
         replicator.stop()
-        // end::multipeer-replicator-stop
+        // end::multipeer-replicator-stop[]
     }
-    
+
     func statusListener() throws {
         let replicator = try createMultipeerReplicator()
-        // tag::multipeer-status-listener
+        // tag::multipeer-status-listener[]
         let token = replicator.addStatusListener { status in
             let state = status.active ? "active" : "inactive"
             let error = status.error?.localizedDescription ?? "none"
             print("Multipeer Replicator: \(state), Error: \(error)")
         }
-        // end::multipeer-status-listener
+        // end::multipeer-status-listener[]
     }
-    
+
     func peerDiscoveryListener() throws {
         let replicator = try createMultipeerReplicator()
-        // tag::multipeer-peer-discovery-listener
+        // tag::multipeer-peer-discovery-listener[]
         let token = replicator.addPeerDiscoveryStatusListener { status in
             let online = status.online ? "online" : "offline"
             print("Peer Discovery Status - Peer ID: \(status.peerID), Status: \(online)")
         }
-        // end::multipeer-peer-discovery-listener
+        // end::multipeer-peer-discovery-listener[]
     }
-    
+
     func peerReplicatorStatus() throws {
         let replicator = try createMultipeerReplicator()
-        // tag::multipeer-replicator-status-listener
+        // tag::multipeer-replicator-status-listener[]
         let activities = ["stopped", "offline", "connecting", "idle", "busy"]
         let token = replicator.addPeerReplicatorStatusListener { replStatus in
             let direction = replStatus.outgoing ? "outgoing" : "incoming"
@@ -187,12 +187,12 @@ class MultipeerReplicatorSnippets {
                   "Activity: \(activity), " +
                   "Error: \(error)")
         }
-        // end::multipeer-replicator-status-listener
+        // end::multipeer-replicator-status-listener[]
     }
-    
+
     func peerDocumentReplication() throws {
         let replicator = try createMultipeerReplicator()
-        // tag::multipeer-document-replication-listener
+        // tag::multipeer-document-replication-listener[]
         let token = replicator.addPeerDocumentReplicationListener { docRepl in
             let direction = docRepl.isPush ? "Push" : "Pull"
             print("Peer Document Replication - Peer ID: \(docRepl.peerID), Direction: \(direction)")
@@ -205,33 +205,33 @@ class MultipeerReplicatorSnippets {
                       " Error: \(error)")
             }
         }
-        // end::multipeer-document-replication-listener
+        // end::multipeer-document-replication-listener[]
     }
-    
+
     func peerID() throws {
         let replicator = try createMultipeerReplicator()
-        // tag::multipeer-peer-id
+        // tag::multipeer-peer-id[]
         let peerID = replicator.peerID
         print("Peer ID: \(peerID)")
-        // end::multipeer-peer-id
+        // end::multipeer-peer-id[]
     }
-    
+
     func neighborPeers() throws {
         let replicator = try createMultipeerReplicator()
-        // tag::multipeer-neighbor-peers
+        // tag::multipeer-neighbor-peers[]
         let neighborPeers = replicator.neighborPeers
         print("Neighbor Peers:")
         neighborPeers.forEach { peerID in
             print(" \(peerID)")
         }
-        // end::multipeer-neighbor-peers
+        // end::multipeer-neighbor-peers[]
     }
-    
+
     func peerInfo() throws {
         let replicator = try createMultipeerReplicator()
-        // tag::multipeer-peer-info
+        // tag::multipeer-peer-info[]
         let activities = ["stopped", "offline", "connecting", "idle", "busy"]
-        
+
         let printPeerInfo: (PeerInfo) -> Void = { info in
             print("Peer ID: \(info.peerID)")
             print(" Status: \(info.online ? "online" : "offline")")
@@ -240,25 +240,25 @@ class MultipeerReplicatorSnippets {
             neighborPeers.forEach { peerID in
                 print("  \(peerID)")
             }
-            
+
             let replStatus = info.replicatorStatus
             let activity = activities[Int(replStatus.activity.rawValue)]
             let error = replStatus.error?.localizedDescription ?? "none"
             print(" Replicator Status: \(activity), Error: \(error)")
         }
-        
+
         replicator.neighborPeers.forEach { peerID in
             if let peerInfo = replicator.peerInfo(for: peerID) {
                 printPeerInfo(peerInfo)
             }
         }
-        // end::multipeer-peer-info
+        // end::multipeer-peer-info[]
     }
-    
+
     func logging() throws {
-        // tag::multipeer-logdomain
+        // tag::multipeer-logdomain[]
         // Enable verbose console logging for multipeer replicator-related domains only.
         LogSinks.console = ConsoleLogSink(level: .verbose, domains: [.peerDiscovery, .multipeer])
-        // end::multipeer-logdomain
+        // end::multipeer-logdomain[]
     }
 }
