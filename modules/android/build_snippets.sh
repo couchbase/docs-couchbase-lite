95b072180cc9a0cf25fa83a3c9793bc235062c2f
Richard Smedley <rgsmedley@gmail.com>
2025-08-26 15:00:03 +0100

    some initial framework for comments/improvements (#981)
    * some initial framework for comments/improvements
    
    * Update gs-prereqs.adoc
    
    * recommended version?
    
    * interim comment incorporation
    
    * Advantages
    
    * initial uml diagram
    
    * interim halt - moving work to Swift pages
    
    * from Androided doc, added mesh note
    
    * Configuration
    
    * Filling out example text
    
    * Active-passive
    
    * Tidy-up
    
    * Android --> Swift
    
    * mermaid --> graphviz
    
    * diagram is fixed
    
    * no patches yet
    
    * compat updates 3.3
    
    * comment out (fix)
    
    * comment out (fix)
    
    * column fix
    
    * 3.3 release
    
    * dot O
    
    * 3.3 updates
    
    * Java 3.3
    
    * ObjC 3.3
    
    * 3.3 Android
    
    * comment out and point to iOS
    
    * --> Swift
    
    * --> 3.3
    
    * table fix
    
    * Deprecation
    
    * table fix
    
    * table fix
    
    * Add swift and objective-c code snippets for MultipeerReplicator (#988)
    
    * The code snippet file names are MultipeerReplicator.swift (Swift) and MultipeerReplicator.m (Objective-C)
    
    * Update iOS code snippet (#991)
    
    * Updated peerIdentity codesnippet to delete the expired identity before creating a new one.
    
    * Added multipeer-logdomain snippet.
    
    * Fixed Jenkinsfile
    
    * Update page structure (#989)
    
    * restructured page structure and headers
    
    * remove duplicate
    
    * remove duplicate
    
    * restructured headers
    
    * updated restructure headers
    
    * update jenkensfile
    
    * multipeer sync for kotlin android (#994)
    
    * multipeer sync for kotlin android
    
    * update formatting
    
    * add content for config
    
    * update the example code path
    
    * update include code samples tag
    
    * updated to reflect swift code
    
    * Update iOS's authenticatorWithRootCerts() and peerIdentity() code snippet (#998)
    
    * Update authenticatorWithRootCerts and peerIdentity code snippet
    
    Updated authenticatorWithRootCerts and peerIdentity code snippet according to the API changes.
    
    * Fix iOS version in Jenkinsfile
    
    * Multipeer sync p2p docs update (#997)
    
    * updated transport modes
    
    * added what's new and updated android sync
    
    * fix style guide
    
    * remove older versions
    
    * updated texts to reflect new changes
    
    * Release notes 3.3 (#999)
    
    * release notes for android cbl 3.3
    
    * release notes ios cbl-3.3
    
    * updated timeline to august from july
    
    * Add code snippet for creating a self-signed identity (#1001)
    
    * Fix bug not deleting the expired identity in objective-c code snippet (#1002)
    
    * updated the docs, including tips (#1000)
    
    * updated the docs, including tips
    
    * added Bonjour config requirement
    
    * ventilated prose
    
    * multipeer supprt
    
    * fix xml formatting
    
    * move signed into peer identity
    
    * self signed identity tag
    
    * Add multipeer examples for Android (#1003)
    
    * Add multipeer examples for Android
    
    As part of this also add in PR validation to make sure that Android snippets remain buildable, and cleanup some warnings in existing snippets (nothing that shows in the docs will be affected, it's just to make real warnings when building more noticeable)
    
    * Use correct shebang
    
    * Download jq if not present
    
    * connect code examples, fix tag errors
    
    * update release notes
    
    * updated swift release note
    
    * update release months for whats new + release note
    
    * update month to september
    
    ---------
    
    Co-authored-by: Pasin Suriyentrakorn <pasin@couchbase.com>
    Co-authored-by: Fortune Ikechi <ikechifortune@gmail.com>
    Co-authored-by: Fortune Ikechi <fortune.ikechi@couchbase.com>
    Co-authored-by: Jim Borden <jim.borden@couchbase.com>

--- modules/android/build_snippets.sh
+++ modules/android/build_snippets.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-cd $SCRIPT_DIR
-
 ANDROID_DIR=$SCRIPT_DIR
 JAVA_DIR=$SCRIPT_DIR/../java
 
---
+++ modules/objc/examples/code_snippets/MultipeerReplicator.m
@@ -0,0 +1,309 @@
+//
+//  MultipeerReplicator.m
+//  CouchbaseLite
+//
+//  Copyright © 2025 couchbase. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CouchbaseLite/CouchbaseLite.h>
+
+@interface MultipeerReplicatorSnippets : NSObject
+
+@end
+
+@interface CustomConflictResolver : NSObject <CBLMultipeerConflictResolver>
+@end
+
+@implementation CustomConflictResolver
+
+- (nullable CBLDocument*) resolveConflict: (CBLConflict*)conflict forPeer: (CBLPeerID*)peerID {
+    return conflict.remoteDocument;
+}
+
+@end
+
+@implementation MultipeerReplicatorSnippets {
+    CBLDatabase *database;
+    CBLCollection *collection1;
+    CBLCollection *collection2;
+    CBLCollection *collection3;
+}
+
+- (NSArray<CBLMultipeerCollectionConfiguration *> *)collectionSimple {
+    // tag::multipeer-collection-simple
+    NSMutableArray<CBLMultipeerCollectionConfiguration*> *collections = [NSMutableArray array];
+    for (CBLCollection *col in @[collection1, collection2, collection3]) {
+        CBLMultipeerCollectionConfiguration *config =
+            [[CBLMultipeerCollectionConfiguration alloc] initWithCollection:col];
+        [collections addObject:config];
+    }
+    // end::multipeer-collection-simple
+    return collections;
+}
+
+- (NSArray<CBLMultipeerCollectionConfiguration *> *)collectionConfig {
+    // Config with custom conflict resolver
+    CBLMultipeerCollectionConfiguration *config1 =
+        [[CBLMultipeerCollectionConfiguration alloc] initWithCollection:collection1];
+    config1.conflictResolver = [[CustomConflictResolver alloc] init];
+
+    // Config with document IDs filter
+    CBLMultipeerCollectionConfiguration *config2 =
+        [[CBLMultipeerCollectionConfiguration alloc] initWithCollection:collection2];
+    config2.documentIDs = @[@"doc1", @"doc2"];
+
+    // Config with push replication filter
+    CBLMultipeerCollectionConfiguration *config3 =
+        [[CBLMultipeerCollectionConfiguration alloc] initWithCollection:collection3];
+    config3.pushFilter = ^BOOL(CBLPeerID *peerID, CBLDocument *document, CBLDocumentFlags flags) {
+        return [document integerForKey:@"access-level"] == 2;
+    };
+
+    NSArray *collections = @[config1, config2, config3];
+    // end::multipeer-collection-config
+    return collections;
+}
+
+- (CBLTLSIdentity *)createSelfSignedIdentity {
+    // tag::multipeer-selfsigned-tlsidentity
+    // Note: This example is simplified for demonstration and does not include error handling.
+    NSString *persistentLabel = @"com.myapp.identity";
+    
+    // Retrieve the TLS identity from the keychain using the persistent label.
+    NSError *error = nil;
+    CBLTLSIdentity *identity = [CBLTLSIdentity identityWithLabel:persistentLabel error:&error];
+    
+    // If the identity exists but is expired, delete it.
+    if (identity && [identity.expiration compare:[NSDate date]] == NSOrderedAscending) {
+        [CBLTLSIdentity deleteIdentityWithLabel: persistentLabel error: &error];
+        identity = nil;
+    }
+        
+    // If the identity doesn't exist or expired, create a new one.
+    if (!identity) {
+        // Define certificate attributes and expiration date.
+        NSDictionary *attrs = @{ kCBLCertAttrCommonName: @"MyApp" };
+        NSDate *expiration = [[NSCalendar currentCalendar] dateByAddingUnit:NSCalendarUnitYear
+                                                                      value:2
+                                                                     toDate:[NSDate date]
+                                                                    options:0];
+        
+        // Create and store a new self-signed identity in the keychain with a persistent label.
+        identity = [CBLTLSIdentity createIdentityForKeyUsages:kCBLKeyUsagesClientAuth|kCBLKeyUsagesServerAuth
+                                                   attributes:attrs
+                                                   expiration:expiration
+                                                        label:persistentLabel
+                                                        error:&error];
+    }
+    // end::multipeer-selfsigned-tlsidentity
+    return identity;
+}
+
+- (CBLTLSIdentity *)createCASignedIdentity {
+    // tag::multipeer-tlsidentity
+    // Note: This example is simplified for demonstration and does not include error handling.
+    NSString *persistentLabel = @"com.myapp.identity";
+    
+    // Retrieve the TLS identity from the keychain using the persistent label.
+    NSError *error = nil;
+    CBLTLSIdentity *identity = [CBLTLSIdentity identityWithLabel:persistentLabel error:&error];
+    
+    // If the identity exists but is expired, delete it.
+    if (identity && [identity.expiration compare:[NSDate date]] == NSOrderedAscending) {
+        [CBLTLSIdentity deleteIdentityWithLabel: persistentLabel error: &error];
+        identity = nil;
+    }
+        
+    // If the identity doesn't exist or expired, create a new one.
+    if (!identity) {
+        // Get the issuer's private key and certificate data (DER format) for signing the identity's certificate.
+        NSData *caKey = [self getIssuerPrivateKeyData];
+        NSData *caCert = [self getIssuerCertificateData];
+        
+        // Define certificate attributes and expiration date.
+        NSDictionary *attrs = @{ kCBLCertAttrCommonName: @"MyApp" };
+        NSDate *expiration = [[NSCalendar currentCalendar] dateByAddingUnit:NSCalendarUnitYear
+                                                                      value:2
+                                                                     toDate:[NSDate date]
+                                                                    options:0];
+        
+        // Create and store a new identity signed with the issuer in the keychain with a persistent label.
+        identity = [CBLTLSIdentity createSignedIdentityInsecureForKeyUsages:kCBLKeyUsagesClientAuth|kCBLKeyUsagesServerAuth
+                                                                 attributes:attrs
+                                                                 expiration:expiration
+                                                                      caKey:caKey
+                                                              caCertificate:caCert
+                                                                      label:persistentLabel
+                                                                      error:&error];
+    }
+    // end::multipeer-tlsidentity
+    return identity;
+}
+
+- (NSData *)getIssuerPrivateKeyData {
+    return [[NSData alloc] initWithBase64EncodedString:@"your_base64_encoded_private_key" options:0];
+}
+
+- (NSData *)getIssuerCertificateData {
+    return [[NSData alloc] initWithBase64EncodedString:@"your_base64_encoded_certicate" options:0];
+}
+
+- (id<CBLMultipeerAuthenticator>)authenticatorWithCallback {
+    // tag::multipeer-authenticator-callback
+    id<CBLMultipeerAuthenticator> authenticator =
+    [[CBLMultipeerCertificateAuthenticator alloc] initWithBlock:^BOOL(CBLPeerID *peerID, NSArray *certs) {
+        return YES;
+    }];
+    // end::multipeer-authenticator-callback
+    return authenticator;
+}
+
+- (id<CBLMultipeerAuthenticator>)authenticatorWithRootCerts {
+    // tag::multipeer-authenticator-rootcerts
+    // Get issuer's certificate data (DER format), which was used to sign the peer's certificate.
+    NSData *caCert = [self getIssuerCertificateData];
+    SecCertificateRef caCertRef = SecCertificateCreateWithData(NULL, (__bridge CFDataRef)caCert);
+    id<CBLMultipeerAuthenticator> authenticator =
+    [[CBLMultipeerCertificateAuthenticator alloc] initWithRootCerts:@[(__bridge_transfer id)caCertRef]];
+    // end::multipeer-authenticator-rootcerts
+    return authenticator;
+}
+
+- (CBLMultipeerReplicatorConfiguration *)createConfig {
+    CBLTLSIdentity *identity = [self createCASignedIdentity];
+    id<CBLMultipeerAuthenticator> authenticator = [self authenticatorWithRootCerts];
+    NSArray<CBLMultipeerCollectionConfiguration *> *collections = [self collectionConfig];
+    
+    // tag::multipeer-config
+    CBLMultipeerReplicatorConfiguration *config =
+    [[CBLMultipeerReplicatorConfiguration alloc] initWithPeerGroupID:@"com.myapp"
+                                                            identity:identity
+                                                       authenticator:authenticator
+                                                         collections:collections];
+    // end::multipeer-config
+    return config;
+}
+
+- (CBLMultipeerReplicator *)createMultipeerReplicator {
+    CBLMultipeerReplicatorConfiguration *config = [self createConfig];
+    NSError *error = nil;
+    // tag::multipeer-replicator
+    CBLMultipeerReplicator *replicator = [[CBLMultipeerReplicator alloc] initWithConfig:config error:&error];
+    // end::multipeer-replicator
+    return replicator;
+}
+
+- (void)startReplicator {
+    CBLMultipeerReplicator *replicator = [self createMultipeerReplicator];
+    // tag::multipeer-replicator-start
+    [replicator start];
+    // end::multipeer-replicator-start
+}
+
+- (void)stopReplicator {
+    CBLMultipeerReplicator *replicator = [self createMultipeerReplicator];
+    // tag::multipeer-replicator-stop
+    [replicator stop];
+    // end::multipeer-replicator-stop
+}
+
+- (void)statusListener {
+    CBLMultipeerReplicator *replicator = [self createMultipeerReplicator];
+    // tag::multipeer-status-listener
+    [replicator addStatusListenerWithQueue:nil listener:^(CBLMultipeerReplicatorStatus *status) {
+        NSString *state = status.active ? @"active" : @"inactive";
+        NSString *err = status.error ? status.error.localizedDescription : @"none";
+        NSLog(@"Multipeer Replicator Status: %@, Error: %@", state, err);
+    }];
+    // end::multipeer-status-listener
+}
+
+- (void)peerDiscoveryListener {
+    CBLMultipeerReplicator *replicator = [self createMultipeerReplicator];
+    // tag::multipeer-peer-discovery-listener
+    [replicator addPeerDiscoveryStatusListenerWithQueue:nil listener:^(CBLPeerDiscoveryStatus *status) {
+        NSString *online = status.online ? @"online" : @"offline";
+        NSLog(@"Peer Discovery Status - Peer ID: %@, Status: %@", status.peerID, online);
+    }];
+    // end::multipeer-peer-discovery-listener
+}
+
+- (void)peerReplicatorStatus {
+    CBLMultipeerReplicator *replicator = [self createMultipeerReplicator];
+    // tag::multipeer-replicator-status-listener
+    NSArray<NSString *> *activities = @[ @"stopped", @"offline", @"connecting", @"idle", @"busy" ];
+    [replicator addPeerReplicatorStatusListenerWithQueue:nil listener:^(CBLPeerReplicatorStatus *replStatus) {
+        NSString *direction = replStatus.outgoing ? @"outgoing" : @"incoming";
+        NSString *activity = activities[replStatus.status.activity];
+        NSString *error = replStatus.status.error ? replStatus.status.error.localizedDescription : @"none";
+        NSLog(@"Peer Replicator Status - "
+              "Peer ID: %@, Direction: %@, Activity: %@, Error: %@",
+              replStatus.peerID, direction, activity, error);
+    }];
+    // end::multipeer-replicator-status-listener
+}
+
+- (void)peerDocumentReplication {
+    CBLMultipeerReplicator *replicator = [self createMultipeerReplicator];
+    // tag::multipeer-document-replication-listener
+    [replicator addPeerDocumentReplicationListenerWithQueue:nil listener:^(CBLPeerDocumentReplication *docRepl) {
+        NSString *direction = docRepl.isPush ? @"Push" : @"Pull";
+        NSLog(@"Peer Document Replication - Peer ID: %@, Direction: %@", docRepl.peerID, direction);
+        for (CBLReplicatedDocument *doc in docRepl.documents) {
+            NSString *error = doc.error ? doc.error.localizedDescription : @"none";
+            NSString *collection = [NSString stringWithFormat:@"%@.%@", doc.scope, doc.collection];
+            NSLog(@" Collection: %@ Document ID: %@, Flags: %lu, Error: %@",
+                  collection, doc.id, (unsigned long)doc.flags, error);
+        }
+    }];
+    // end::multipeer-document-replication-listener
+}
+
+- (void)peerID {
+    CBLMultipeerReplicator *replicator = [self createMultipeerReplicator];
+    // tag::multipeer-peer-id
+    CBLPeerID *peerID = replicator.peerID;
+    NSLog(@"Peer ID: %@", peerID);
+    // end::multipeer-peer-id
+}
+
+- (void)neighborPeers {
+    CBLMultipeerReplicator *replicator = [self createMultipeerReplicator];
+    // tag::multipeer-neighbor-peers
+    NSLog(@"Neighbor Peers:");
+    for (CBLPeerID *peerID in replicator.neighborPeers) {
+        NSLog(@" %@", peerID);
+    }
+    // end::multipeer-neighbor-peers
+}
+
+- (void)peerInfo {
+    CBLMultipeerReplicator *replicator = [self createMultipeerReplicator];
+    // tag::multipeer-peer-info
+    NSArray<NSString *> *activities = @[ @"stopped", @"offline", @"connecting", @"idle", @"busy" ];
+    
+    void (^printPeerInfo)(CBLPeerInfo *) = ^(CBLPeerInfo *info) {
+        NSLog(@"Peer ID: %@", info.peerID);
+        NSLog(@" Status: %@", info.online ? @"online" : @"offline");
+        NSLog(@" Neighbor Peers:");
+        for (CBLPeerID *peerID in info.neighborPeers) {
+            NSLog(@"  %@", peerID);
+        }
+        
+        CBLReplicatorStatus *replStatus = info.replicatorStatus;
+        NSString *activity = activities[(NSInteger)replStatus.activity];
+        NSString *error = replStatus.error ? replStatus.error.localizedDescription : @"none";
+        NSLog(@" Replicator Status: %@, Error: %@", activity, error);
+    };
+    
+    for (CBLPeerID *peerID in replicator.neighborPeers) {
+        CBLPeerInfo *peerInfo = [replicator peerInfoForPeerID: peerID];
+        if (peerInfo) {
+            printPeerInfo(peerInfo);
+        }
+    }
+    // end::multipeer-peer-info
+}
+
+@end
