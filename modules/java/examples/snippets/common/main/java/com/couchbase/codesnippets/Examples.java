9012104816e73c45d0626852d675a2a597729849
Fortune Ikechi <fortune.ikechi@couchbase.com>
2026-01-09 14:43:20 +0000

    update logging api in android for 3.3

--- modules/java/examples/snippets/common/main/java/com/couchbase/codesnippets/Examples.java
+++ modules/java/examples/snippets/common/main/java/com/couchbase/codesnippets/Examples.java
@@ -154,6 +154,7 @@ public class Examples {
     public void newFileLoggingExample() {
         // tag::new-file-logging[]
         LogSinks.get().setFile(new FileLogSink.Builder()
+            .setLevel(LogLevel.VERBOSE)
             .setDirectory("/tmp/logs")
             .setMaxKeptFiles(12)
             .setPlainText(false)
