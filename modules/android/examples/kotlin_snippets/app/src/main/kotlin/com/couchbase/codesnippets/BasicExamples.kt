fd47a18b36f8682581ac7736838dd30eb45b010b
Fortune Ikechi <fortune.ikechi@couchbase.com>
2026-01-09 13:50:14 +0000

    update the new logging api for 3.2 android

--- modules/android/examples/kotlin_snippets/app/src/main/kotlin/com/couchbase/codesnippets/BasicExamples.kt
+++ modules/android/examples/kotlin_snippets/app/src/main/kotlin/com/couchbase/codesnippets/BasicExamples.kt
@@ -206,9 +206,11 @@ class BasicExamples(private val context: Context) {
     fun newFileLoggingExample() {
         // tag::new-file-logging[]
         FileLogSinkFactory.install(
+            level = LogLevel.VERBOSE,
             directory = "/tmp/logs",
             maxKeptFiles = 12,
-            isPlainText = true)
+            isPlainText = true
+        )
         // end::new-file-logging[]
     }
 
