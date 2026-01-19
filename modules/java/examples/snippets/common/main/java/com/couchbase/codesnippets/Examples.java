4f0a52eaa7a25c2f6cd9a72f392c0c7c14ea4b67
Fortune Ikechi <ikechifortune@gmail.com>
2026-01-19 13:32:58 +0000

    update the new logging api for 3.2 android (#1066)
    * update the new logging api for 3.2 android
    
    * update the jenkensfile version

--- modules/java/examples/snippets/common/main/java/com/couchbase/codesnippets/Examples.java
+++ modules/java/examples/snippets/common/main/java/com/couchbase/codesnippets/Examples.java
@@ -153,7 +153,8 @@ public class Examples {
 
     public void newFileLoggingExample() {
         // tag::new-file-logging[]
-        LogSinks.get().setFile(new FileLogSink.Builder()
+         LogSinks.get().setFile(new FileLogSink.Builder()
+            .setLevel(LogLevel.VERBOSE)
             .setDirectory("/tmp/logs")
             .setMaxKeptFiles(12)
             .setPlainText(false)
