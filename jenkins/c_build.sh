2784b080b8bc24ce7cccb52bff4f484721f73463
Pasin Suriyentrakorn <pasin@couchbase.com>
2026-04-01 09:04:28 -0700

    Fix Jenkins PR Validation (#1087)
    * iOS :
       - Fixed and simplify script to download the right CBL and VS binary.
       - Fixed one error in code snippet.
    
    * C :
       - Applied the same method as iOS get the lastest CBL version.
       - Removed obsolete logging code snippet as the API has been changed in 4.0
    
    *  Android:
       - Updated AGP from 8.0.2 to 8.13.2 to fix JDK 21 incompatibility issue.
       - Update Android script to get CBL and VS version based on isReleased flag.

--- jenkins/c_build.sh
+++ jenkins/c_build.sh
@@ -2,23 +2,41 @@
 
 if [[ "$#" -ne 1 ]]; then
     echo "This script requires a version argument"
+    exit 1
 fi
 
-THIS_DIR=$( dirname -- $(realpath "$0"); )
-version="$1"
+THIS_DIR=$( dirname -- $(realpath "$0") )
+CBL_VERSION="$1"
 pushd "$THIS_DIR/../modules/c/examples/code_snippets"
 
-build_num=$(curl -s http://dbapi.build.couchbase.com:8000/v1/products/couchbase-lite-c/releases/$version/versions/$version/builds?filter=last_complete | jq .build_num | bc)
+RESPONSE=$(curl -s "http://proget.build.couchbase.com:8080/api/get_version?product=couchbase-lite-c&version=${CBL_VERSION}")
+CBL_IS_RELEASE=$(echo -n "$RESPONSE" | jq .IsRelease)
+CBL_BUILD_NO=$(echo -n "$RESPONSE" | jq .BuildNumber)
+
+if [ "${CBL_BUILD_NO}" == "" ] || [ "${CBL_BUILD_NO}" == "null" ]; then
+    echo "No latest successful build found for CBL v${CBL_VERSION}"
+    exit 3
+fi
+
+if [ "${CBL_IS_RELEASE}" == "true" ]; then
+    PACKAGE_NAME="couchbase-lite-c-enterprise-${CBL_VERSION}-linux-x86_64.tar.gz"
+    DOWNLOAD_URL="https://latestbuilds.service.couchbase.com/builds/releases/mobile/couchbase-lite-c/${CBL_VERSION}/${PACKAGE_NAME}"
+else
+    PACKAGE_NAME="couchbase-lite-c-enterprise-${CBL_VERSION}-${CBL_BUILD_NO}-linux-x86_64.tar.gz"
+    DOWNLOAD_URL="https://latestbuilds.service.couchbase.com/builds/latestbuilds/couchbase-lite-c/${CBL_VERSION}/${CBL_BUILD_NO}/${PACKAGE_NAME}"
+fi
+
+rm -rf downloaded
 mkdir -p downloaded
 
 pushd downloaded
 DOWNLOAD_DIR=$(pwd)
-wget http://latestbuilds.service.couchbase.com/builds/latestbuilds/couchbase-lite-c/$version/$build_num/couchbase-lite-c-enterprise-$version-$build_num-linux-x86_64.tar.gz
-tar xf couchbase-lite-c-enterprise-$version-$build_num-linux-x86_64.tar.gz
-rm couchbase-lite-c-enterprise-$version-$build_num-linux-x86_64.tar.gz
+wget "${DOWNLOAD_URL}"
+tar xf "${PACKAGE_NAME}"
+rm "${PACKAGE_NAME}"
 popd
 
 mkdir -p build
 pushd build
-cmake -DCMAKE_PREFIX_PATH="$DOWNLOAD_DIR/libcblite-$version" ..
+cmake -DCMAKE_PREFIX_PATH="$DOWNLOAD_DIR/libcblite-${CBL_VERSION}" ..
 make -j12
